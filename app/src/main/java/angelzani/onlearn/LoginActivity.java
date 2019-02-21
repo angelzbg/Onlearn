39a78c0f8aff2ae78b02fec0654722d9a49e0550
angelzbg <badblo0dbg@gmail.com>
2019-02-21 21:57:15 +0200

    Update LoginActivity.java

--- app/src/main/java/angelzani/onlearn/LoginActivity.java
+++ app/src/main/java/angelzani/onlearn/LoginActivity.java
@@ -124,25 +124,6 @@ public class LoginActivity extends AppCompatActivity {
 
     /*----- OnClickListeners [  END  ] -----*/
 
-    private void registerNewUser(final String email, final String password) {
-        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    // Sign in success, update UI with the signed-in user's information
-                    //Log.d(TAG, "createUserWithEmail:success");
-                    FirebaseUser user = mAuth.getCurrentUser();
-                    //updateUI(user);
-                } else {
-                    // If sign in fails, display a message to the user.
-                    //Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                    Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
-                    //updateUI(null);
-                }
-            }
-        });
-    }//end registerNewUser()
-
     private void login(final String email, final String password) {
         mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
