3cf654c6d83fa784e434b54191760cefff31b37b
angelzbg <badblo0dbg@gmail.com>
2019-02-23 16:32:49 +0200

    blegh

--- app/src/main/java/angelzani/onlearn/ClientActivity.java
+++ app/src/main/java/angelzani/onlearn/ClientActivity.java
@@ -1,13 +1,78 @@
 package angelzani.onlearn;
 
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.graphics.Point;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ClientActivity extends AppCompatActivity {
+
+    //Display metrics
+    private int width, height;
+
+    //Firebase
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mRef, dbRefUsers, dbRefCourse, dbRefGroups, dbRefParticipation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT); // Екрана в нормално състояние без да се завърта повече
         setContentView(R.layout.activity_client);
+        initializeUI();
+
+        //Firebase
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance();
+        mRef = mDatabase.getReference(); //root
+        dbRefUsers = mRef.child("users");
+        dbRefCourse = mRef.child("course");
+        dbRefGroups = mRef.child("groups");
+        dbRefParticipation = mRef.child("participation");
+
+    }//end onCreate()
+
+    private void initializeUI(){
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        width = size.x;
+        height = size.y;
+
+        /* ----- Header Layout ----- */
+        findViewById(R.id.client_CL_Head).getLayoutParams().height = height/16;
+        //Profile Icon
+        findViewById(R.id.client_IV_Profile).getLayoutParams().width = height/18;
+        findViewById(R.id.client_IV_Profile).getLayoutParams().height = findViewById(R.id.client_IV_Profile).getLayoutParams().width;
+        setMargins(findViewById(R.id.client_IV_Profile), height/80,0,height/80,0);
+
+    }
+
+    //Utility
+    private boolean isInternetAvailable() {
+        ConnectivityManager cm = (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+        //if(!isConnected) showAlert("Alert", "No internet connection.");
+        return isConnected;
+    }
+
+    private void setMargins (View v, int left, int top, int right, int bottom) {
+        if (v.getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
+            ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
+            p.setMargins(left, top, right, bottom);
+            v.requestLayout();
+        }
     }
 }
