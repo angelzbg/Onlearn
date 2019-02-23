6b8681f6692b9dbd98d247609467a2b4afebf522
Aneliya Angelova <38349637+AneliyaA@users.noreply.github.com>
2019-02-23 20:08:44 +0200

    admin activity

--- app/src/main/java/angelzani/onlearn/AdminActivity.java
+++ app/src/main/java/angelzani/onlearn/AdminActivity.java
@@ -1,19 +1,28 @@
 package angelzani.onlearn;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Point;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AdminActivity extends AppCompatActivity {
 
@@ -22,6 +31,7 @@ public class AdminActivity extends AppCompatActivity {
 
     //Firebase
     private FirebaseAuth mAuth;
+    private FirebaseUser user;
     private FirebaseDatabase mDatabase;
     private DatabaseReference mRef, dbRefUsers, dbRefCourse, dbRefGroups, dbRefParticipation;
 
@@ -35,6 +45,7 @@ public class AdminActivity extends AppCompatActivity {
 
         //Firebase
         mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
         mDatabase = FirebaseDatabase.getInstance();
         mRef = mDatabase.getReference(); //root
         dbRefUsers=mRef.child("users");
@@ -52,12 +63,47 @@ public class AdminActivity extends AppCompatActivity {
         width = size.x;
         height = size.y;
 
+        int _20px=height/40;
+
         //Header Layout
         findViewById(R.id.admin_CL_header).getLayoutParams().height=height/16;
+
+
         //Profile Icon
         findViewById(R.id.admin_IV_profile).getLayoutParams().height=height/18;
         findViewById(R.id.admin_IV_profile).getLayoutParams().width=height/18;
         setMargins(findViewById(R.id.admin_IV_profile),height/80,0,height/80,0);
+        findViewById(R.id.admin_LL_toProfile).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AdminActivity.this, ProfileActivity.class));
+            }
+        });
+
+        //Name TextView
+        ((TextView)findViewById(R.id.admin_TV_name)).setTextSize(TypedValue.COMPLEX_UNIT_PX, _20px);
+        dbRefUsers.child(user.getUid()).child("name").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists())
+                {
+                    ((TextView)findViewById(R.id.admin_TV_name)).setText(dataSnapshot.getValue(String.class));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                if(!isInternetAvailable())
+                {
+                    Toast.makeText(getApplicationContext(),"No internet connection.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        /*------ HeadMenu TAB ------*/
+        findViewById(R.id.admin_LL_headMenu).getLayoutParams().height=(int)(height/14.5);
+        ((TextView)findViewById(R.id.admin_TV_HM_manage)).setTextSize(TypedValue.COMPLEX_UNIT_PX, _20px);
+        ((TextView)findViewById(R.id.admin_TV_HM_add)).setTextSize(TypedValue.COMPLEX_UNIT_PX, _20px);
     }
 
     //Utility
