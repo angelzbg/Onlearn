429e799a2a965f020d08d2af0129a077acec0bc3
Aneliya Angelova <38349637+AneliyaA@users.noreply.github.com>
2019-02-23 16:32:20 +0200

    adasd

--- app/src/main/java/angelzani/onlearn/AdminActivity.java
+++ app/src/main/java/angelzani/onlearn/AdminActivity.java
@@ -1,13 +1,79 @@
 package angelzani.onlearn;
 
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.graphics.Point;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AdminActivity extends AppCompatActivity {
+
+    //Display metrics
+    private int width, height;
+
+    //Firebase
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mRef, dbRefUsers, dbRefCourse, dbRefGroups, dbRefParticipation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
         setContentView(R.layout.activity_admin);
+        initializeUI();
+
+        //Firebase
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance();
+        mRef = mDatabase.getReference(); //root
+        dbRefUsers=mRef.child("users");
+        dbRefCourse=mRef.child("course");
+        dbRefGroups=mRef.child("groups");
+        dbRefParticipation=mRef.child("participation");
+
+    }//end onCreate()
+
+    private void initializeUI()
+    {
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        width = size.x;
+        height = size.y;
+
+        //Header Layout
+        findViewById(R.id.admin_CL_header).getLayoutParams().height=height/16;
+        //Profile Icon
+        findViewById(R.id.admin_IV_profile).getLayoutParams().height=height/18;
+        findViewById(R.id.admin_IV_profile).getLayoutParams().width=height/18;
+        setMargins(findViewById(R.id.admin_IV_profile),height/80,0,height/80,0);
+    }
+
+    //Utility
+    private boolean isInternetAvailable() {
+        ConnectivityManager cm = (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+        //if(!isConnected) showAlert("Alert", "No internet connection.");
+        return isConnected;
+    }
+
+    private void setMargins (View v, int left, int top, int right, int bottom) {
+        if (v.getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
+            ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
+            p.setMargins(left, top, right, bottom);
+            v.requestLayout();
+        }
     }
 }
