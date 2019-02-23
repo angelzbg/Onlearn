f456662158af67d926e241b82e3a4d4f4c450f04
angelzbg <badblo0dbg@gmail.com>
2019-02-23 14:44:45 +0200

    turbo qk dizain na loading anim & alert box

--- app/src/main/java/angelzani/onlearn/LoginActivity.java
+++ app/src/main/java/angelzani/onlearn/LoginActivity.java
@@ -3,15 +3,21 @@ package angelzani.onlearn;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Point;
 import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.GradientDrawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.constraint.ConstraintSet;
@@ -20,6 +26,9 @@ import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.Display;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -69,7 +78,6 @@ public class LoginActivity extends AppCompatActivity {
         FirebaseUser user = mAuth.getCurrentUser();
         if(user!=null){ // логнати сме
             //теглим атрибут role и роверяваме дали е client или admin -> ClientActivity || AdminActivity
-            showProgress();
             mRef.child("users").child(user.getUid()).child("role").addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -79,12 +87,11 @@ public class LoginActivity extends AppCompatActivity {
                 @Override
                 public void onCancelled(@NonNull DatabaseError databaseError) {
                     if(!isInternetAvailable()) {
-                        hideProgress();
-                        Toast.makeText(getApplicationContext(), "No Internet Connection.", Toast.LENGTH_LONG).show();
+                        //Toast.makeText(getApplicationContext(), "No Internet Connection.", Toast.LENGTH_LONG).show();
                     }
                 }
             });
-        } else hideProgress();
+        }
     }//end onStart()
 
     private void initializeUI(){
@@ -130,12 +137,43 @@ public class LoginActivity extends AppCompatActivity {
         cs.connect(R.id.login_TV_Register, ConstraintSet.TOP, R.id.login_TI_Password, ConstraintSet.BOTTOM, _20px*2);
         cs.applyTo((ConstraintLayout)findViewById(R.id.login_CL_RegisterBox));
 
-        findViewById(R.id.login_IV_LoadingBar).getLayoutParams().width = width/5;
+        // Loading Progress Anim
+        findViewById(R.id.login_IV_LoadingBar).getLayoutParams().width = width/4;
         findViewById(R.id.login_IV_LoadingBar).getLayoutParams().height = findViewById(R.id.login_IV_LoadingBar).getLayoutParams().width;
 
+        // Alert Message
+        findViewById(R.id.login_CL_Alert).setPadding(height/80,height/80,height/80,height/80);
+
+        gradientDrawable = new GradientDrawable();
+        gradientDrawable.setColor(Color.parseColor("#E9FFFFFF"));
+        gradientDrawable.setShape(GradientDrawable.RECTANGLE);
+        gradientDrawable.setCornerRadius(_20px/2);
+        findViewById(R.id.login_CL_AlertBox).setBackground(gradientDrawable);
+
+        ((TextView)findViewById(R.id.login_TV_AlertTitle)).setTextSize(TypedValue.COMPLEX_UNIT_PX, height/33);
+        findViewById(R.id.login_TV_AlertTitle).setPadding(0,height/80,0,height/80);
+
+        ((TextView)findViewById(R.id.login_TV_AlertMessage)).setTextSize(TypedValue.COMPLEX_UNIT_PX, _20px);
+        findViewById(R.id.login_TV_AlertMessage).setPadding(_20px,_20px,_20px,_20px);
+
+        ((TextView)findViewById(R.id.login_TV_AlertClose)).setTextSize(TypedValue.COMPLEX_UNIT_PX, _20px);
+        findViewById(R.id.login_TV_AlertClose).getLayoutParams().width = height/16;
+        findViewById(R.id.login_TV_AlertClose).getLayoutParams().height = findViewById(R.id.login_TV_AlertClose).getLayoutParams().width;
+        setMargins(findViewById(R.id.login_TV_AlertClose), 0,0,0,height/80);
+        gradientDrawable = new GradientDrawable();
+        gradientDrawable.setColor(Color.parseColor("#E5EEFC"));
+        gradientDrawable.setShape(GradientDrawable.OVAL);
+        gradientDrawable.setStroke(1, Color.parseColor("#82B1FF"));
+        findViewById(R.id.login_TV_AlertClose).setBackground(gradientDrawable);
+
+        //Elevations за lollipop+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             findViewById(R.id.login_CL_RegisterWrap).setElevation(_20px/2);
             findViewById(R.id.login_TV_Logo).setElevation(_20px/2);
+            findViewById(R.id.login_CL_Loading).setElevation(_20px);
+            findViewById(R.id.login_CL_Alert).setElevation(_20px*2);
+
+            findViewById(R.id.login_CL_AlertBox).setElevation(_20px/4);
         }
 
         animationLoading = (AnimationDrawable) findViewById(R.id.login_IV_LoadingBar).getBackground();
@@ -145,6 +183,12 @@ public class LoginActivity extends AppCompatActivity {
         //On Click Listeners
         findViewById(R.id.login_TV_Register).setOnClickListener(goToRegister);
         findViewById(R.id.login_TV_Login).setOnClickListener(loginListener);
+        findViewById(R.id.login_TV_AlertClose).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.login_CL_Alert).setVisibility(View.INVISIBLE);
+            }
+        });
     }
 
     /*----- OnClickListeners [ START ] -----*/
@@ -165,18 +209,20 @@ public class LoginActivity extends AppCompatActivity {
 
             final String email = ((EditText)findViewById(R.id.login_ET_Email)).getText().toString().trim();
             if(!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-                Toast.makeText(getApplicationContext(), "Please enter valid email address.", Toast.LENGTH_LONG).show();
+                showAlert("Alert", "Email must be valid.");
+                //Toast.makeText(getApplicationContext(), "Please enter valid email address.", Toast.LENGTH_LONG).show();
                 return;
             }
 
             final String password = ((EditText)findViewById(R.id.login_ET_Password)).getText().toString().trim();
             if(password.length() < 6) {
-                Toast.makeText(getApplicationContext(), "Password must contain at least 6 characters.", Toast.LENGTH_LONG).show();
+                showAlert("Alert", "Password must contain at least 6 characters.");
+                //Toast.makeText(getApplicationContext(), "Password must contain at least 6 characters.", Toast.LENGTH_LONG).show();
                 return;
             }
 
             if(!isInternetAvailable()) {
-                Toast.makeText(getApplicationContext(), "No Internet Connection.", Toast.LENGTH_LONG).show();
+                //Toast.makeText(getApplicationContext(), "No Internet Connection.", Toast.LENGTH_LONG).show();
                 return;
             }
 
@@ -210,7 +256,7 @@ public class LoginActivity extends AppCompatActivity {
                         public void onCancelled(@NonNull DatabaseError databaseError) {
                             if(!isInternetAvailable()) {
                                 hideProgress();
-                                Toast.makeText(getApplicationContext(), "No Internet Connection.", Toast.LENGTH_LONG).show();
+                                //Toast.makeText(getApplicationContext(), "No Internet Connection.", Toast.LENGTH_LONG).show();
                             }
                         }
                     });
@@ -219,7 +265,8 @@ public class LoginActivity extends AppCompatActivity {
                     // If sign in fails, display a message to the user.
                     //Log.w(TAG, "signInWithEmail:failure", task.getException());
                     hideProgress();
-                    Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                    showAlert("Error", task.getException().getMessage());
+                    //Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
                     //updateUI(null);
                 }
             }
@@ -239,12 +286,60 @@ public class LoginActivity extends AppCompatActivity {
         //}
     }
 
+    private void showAlert(final String title, final String message){
+        ((TextView)findViewById(R.id.login_TV_AlertTitle)).setText(title);
+        ((TextView)findViewById(R.id.login_TV_AlertMessage)).setText(message);
+
+        findViewById(R.id.login_CL_Alert).setBackground(new BitmapDrawable(getResources(), createBlurBitmapFromScreen()));
+        findViewById(R.id.login_CL_Alert).setVisibility(View.VISIBLE);
+        Animation expandIn = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.expand_in);
+        findViewById(R.id.login_CL_AlertBox).startAnimation(expandIn);
+    }
+
+    private Bitmap createBlurBitmapFromScreen() {
+        Bitmap bitmap = null;
+        findViewById(R.id.login_CL_Main).setDrawingCacheEnabled(true);
+        bitmap = Bitmap.createBitmap(findViewById(R.id.login_CL_Main).getDrawingCache());
+        findViewById(R.id.login_CL_Main).setDrawingCacheEnabled(false);
+        bitmap = Bitmap.createScaledBitmap(bitmap, 480, 800, false);
+
+        Bitmap result = null;
+        try {
+            RenderScript rsScript = RenderScript.create(getApplicationContext());
+            Allocation alloc = Allocation.createFromBitmap(rsScript, bitmap);
+
+            ScriptIntrinsicBlur blur = ScriptIntrinsicBlur.create(rsScript, Element.U8_4(rsScript));
+            blur.setRadius(21);
+            blur.setInput(alloc);
+
+            result = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+            Allocation outAlloc = Allocation.createFromBitmap(rsScript, result);
+
+            blur.forEach(outAlloc);
+            outAlloc.copyTo(result);
+
+            rsScript.destroy();
+        } catch (Exception e) {
+            return bitmap;
+        }
+        return result;
+    }
+
     //Utility
     private boolean isInternetAvailable() {
         ConnectivityManager cm = (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
         boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+        if(!isConnected) showAlert("Alert", "No internet connection.");
         return isConnected;
+    }
+
+    private void setMargins (View v, int left, int top, int right, int bottom) {
+        if (v.getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
+            ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
+            p.setMargins(left, top, right, bottom);
+            v.requestLayout();
+        }
     }
 
 }//end LoginActivity{}
