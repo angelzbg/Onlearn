09034b0b314d1e7de9b55de3e75fc4bc9e3322c5
angelzbg <badblo0dbg@gmail.com>
2019-02-24 16:58:37 +0200

    blegh

---
+++ app/src/main/java/angelzani/onlearn/ManageActivity.java
@@ -0,0 +1,94 @@
+package angelzani.onlearn;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.graphics.Point;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Display;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class ManageActivity extends AppCompatActivity {
+
+    //Display metrics
+    private int width, height;
+
+    //Firebase
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mRef, dbRefUsers, dbRefCourse, dbRefGroups, dbRefParticipation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
+        setContentView(R.layout.activity_manage);
+        //Firebase
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance();
+        mRef = mDatabase.getReference(); //root
+        dbRefUsers = mRef.child("users");
+        dbRefCourse = mRef.child("course");
+        dbRefGroups = mRef.child("groups");
+        dbRefParticipation = mRef.child("participation");
+
+        initializeUI();
+
+    }//end onCreate()
+
+    private void initializeUI(){
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        width = size.x;
+        height = size.y;
+
+        int _20px = height/40;
+
+
+
+    }// end of InitializeUI()
+
+    /* ----- OnClickListeners [ START ] ----- */
+
+
+
+    /* ----- OnClickListeners [  END  ] ----- */
+
+    //Utility
+    private boolean isInternetAvailable() {
+        ConnectivityManager cm = (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+        //if(!isConnected) showAlert("Alert", "No internet connection.");
+        return isConnected;
+    }
+
+    private void setMargins (View v, int left, int top, int right, int bottom) {
+        if (v.getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
+            ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
+            p.setMargins(left, top, right, bottom);
+            v.requestLayout();
+        }
+    }
+}
