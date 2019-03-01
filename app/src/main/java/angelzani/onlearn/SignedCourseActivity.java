d3e3969a770cd0e19856976332fda3432c767094
angelzbg <badblo0dbg@gmail.com>
2019-03-01 14:22:30 +0200

    firebase info

--- app/src/main/java/angelzani/onlearn/SignedCourseActivity.java
+++ app/src/main/java/angelzani/onlearn/SignedCourseActivity.java
@@ -4,15 +4,25 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class SignedCourseActivity extends AppCompatActivity { // Калофер
 
@@ -44,7 +54,100 @@ public class SignedCourseActivity extends AppCompatActivity { // Калофер
         final String descriptionExtra = intent.getStringExtra("description");
         final String lecturerIdExtra = intent.getStringExtra("lecturerId");
         final String groupIdExtra = intent.getStringExtra("groupId");
-    }
+
+
+        Toast.makeText(getApplicationContext(), "Course name: " + courseIdExtra, Toast.LENGTH_LONG).show();
+
+        // Изтегляне на позволената информация за потребителят, който е създал дисциплината
+        dbRefUsers.child(lecturerIdExtra).child("name").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //Displaying data
+                Toast.makeText(getApplicationContext(), "Lecturer name: " + dataSnapshot.getValue(String.class), Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getApplicationContext(), databaseError.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        dbRefUsers.child(lecturerIdExtra).child("email").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //Displaying data
+                Toast.makeText(getApplicationContext(), "Lecturer email: " + dataSnapshot.getValue(String.class), Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getApplicationContext(), databaseError.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // Информация за групата
+        dbRefGroups.child(courseIdExtra).child(groupIdExtra).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String groupName = dataSnapshot.getKey(); // groupIdExtra
+                Long startLongDate = dataSnapshot.child("start").getValue(Long.class);
+                Long endLongDate = dataSnapshot.child("end").getValue(Long.class);
+                int signedPeople = dataSnapshot.child("current").getValue(Integer.class);
+                int maxPeople = dataSnapshot.child("max").getValue(Integer.class);
+
+                Date startDate = new Date(startLongDate), endDate = new Date(endLongDate);
+
+                //Displaying data
+                Toast.makeText(getApplicationContext(), "Group name: " + groupName + "\nStart date: " + new SimpleDateFormat("dd/MM/yyyy HH:mm").format(startDate)
+                        + "\nEnd date: " + new SimpleDateFormat("dd/MM/yyyy HH:mm").format(endDate)
+                        + "\nSigned people: " + signedPeople + "/" + maxPeople, Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getApplicationContext(), databaseError.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // Всички айдита на хора, които са записани в тази група в тази дисциплина
+        dbRefParticipation.child(courseIdExtra).orderByValue().equalTo(groupIdExtra).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot DATA) {
+                for(DataSnapshot dataSnapshot : DATA.getChildren()) {
+                    final String userID = dataSnapshot.getKey();
+                    //Toast.makeText(getApplicationContext(), "ID: " + userID, Toast.LENGTH_SHORT).show();
+
+                    // -----
+                    dbRefUsers.child(userID).child("name").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataName) {
+
+                            final String userName = dataName.getValue(String.class);
+
+                            dbRefUsers.child(userID).child("email").addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                    String userEmail = dataSnapshot.getValue(String.class);
+
+                                    //Displaying data
+                                    Toast.makeText(getApplicationContext(), userName + " - " + userEmail, Toast.LENGTH_LONG).show();
+
+                                }
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) { }
+                            });
+
+                        }
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) { }
+                    });
+                    // -----
+                }//for loop
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getApplicationContext(), databaseError.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }// края на onCreate()
 
     //Utility
     private boolean isInternetAvailable() {
